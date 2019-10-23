0688ae759bb456b51358b33f89b673c41be24318
froglegg <hayescrowley@gmail.com>
2019-10-23 19:31:58 -0400

    created client folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import About from "./Views/About";
+import Closet from "./Views/Closet";
+import Home from "./Views/Home";
+import LogIn from "./Views/LogIn";
+import SignUp from "./Views/SignUp";
+import Outfit from "./Views/OutFit";
+import Nav from "./Components/Nav";
+import NoMatch from "./Views/NoMatch";
 
 function App() {
   return (
@@ -11,9 +15,13 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
+          <Route exact path="/" component={LogIn} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Home" component={Home} />
+          <Route exact path="/SignUp" component={SignUp} />
+          <Route exact path="/Closet" component={Closet} />
+          <Route exact path="/Outfit" component={Outfit} />
+          <Route exact path="/Closet/:id" component={Closet} />
           <Route component={NoMatch} />
         </Switch>
       </div>
